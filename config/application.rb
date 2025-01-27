e78442cec4cd04deec9c9f49028cbee7a1206a6f
mike <wl02599509@gmail.com>
2025-01-28 00:27:27 +0800

    setup local default

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module CiHangTempleOfficial
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = "zh-TW"
   end
 end

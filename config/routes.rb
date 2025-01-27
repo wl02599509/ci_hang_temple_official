2cf076b49d3842fc62e436d48403a5c0869588b3
mike <wl02599509@gmail.com>
2025-01-27 21:26:52 +0800

    add devise member

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    devise_for :members, controllers: {
+      sessions: 'admin/members/sessions',
+      registrations: 'admin/members/registrations',
+      passwords: 'admin/members/passwords'
+    }
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_27_131218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "members", force: :cascade do |t|
+    t.string "id_card_number", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["id_card_number"], name: "index_members_on_id_card_number", unique: true
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
 end

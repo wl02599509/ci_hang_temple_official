e79de934d4032e726ff8c29c1f33aba8b16534ea
mike <wl02599509@gmail.com>
2025-01-27 23:05:08 +0800

    new account registration form

--- app/controllers/admin/members/registrations_controller.rb
+++ app/controllers/admin/members/registrations_controller.rb
@@ -5,8 +5,8 @@ module Admin
     class RegistrationsController < Devise::RegistrationsController
       layout 'admin'
 
-      # before_action :configure_sign_up_params, only: [:create]
-      # before_action :configure_account_update_params, only: [:update]
+      before_action :configure_sign_up_params, only: [:create]
+      before_action :configure_account_update_params, only: [:update]
 
       # GET /resource/sign_up
       # def new
@@ -45,19 +45,23 @@ module Admin
       # protected
 
       # If you have extra params to permit, append them to the sanitizer.
-      # def configure_sign_up_params
-      #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-      # end
+      def configure_sign_up_params
+        devise_parameter_sanitizer.permit(:sign_up,
+                                          keys: [:id_card_number, :name, :home_phone_number,
+                                                 :mobile_phone_number, :address, :birthday, :email])
+      end
 
       # If you have extra params to permit, append them to the sanitizer.
-      # def configure_account_update_params
-      #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-      # end
+      def configure_account_update_params
+        devise_parameter_sanitizer.permit(:account_update,
+                                          keys: [:id_card_number, :name, :home_phone_number,
+                                                 :mobile_phone_number, :address, :birthday, :email])
+      end
 
       # The path used after sign up.
-      # def after_sign_up_path_for(resource)
-      #   super(resource)
-      # end
+      def after_sign_up_path_for(resource)
+        admin_dashboard_path
+      end
 
       # The path used after sign up for inactive accounts.
       # def after_inactive_sign_up_path_for(resource)

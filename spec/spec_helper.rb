bd2e5a4786b27e371326691172993236f7446d85
mike <wl02599509@gmail.com>
2025-01-28 16:05:56 +0800

    add rails_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,6 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
-
-abort('The Rails is running in production mode!') if Rails.env.production?
-
-require 'rspec/rails'
-
-require 'shoulda/matchers'
-require 'faker'
-require 'factory_bot'
-require 'sidekiq/testing'
-
-Rails.root.glob('spec/support/**/*.rb').sort.each { |f| require f }
-
 RSpec.configure do |config|
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end

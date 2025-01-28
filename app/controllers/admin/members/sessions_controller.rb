cf79eddc455322d9b310388b4a548daeba5895aa
mike <wl02599509@gmail.com>
2025-01-28 15:11:09 +0800

    sign in feat

--- app/controllers/admin/members/sessions_controller.rb
+++ app/controllers/admin/members/sessions_controller.rb
@@ -5,7 +5,7 @@ module Admin
     class SessionsController < Devise::SessionsController
       layout 'admin'
 
-      # before_action :configure_sign_in_params, only: [:create]
+      before_action :configure_sign_in_params, only: [:create]
 
       # GET /resource/sign_in
       # def new
@@ -25,9 +25,13 @@ module Admin
       # protected
 
       # If you have extra params to permit, append them to the sanitizer.
-      # def configure_sign_in_params
-      #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-      # end
+      def configure_sign_in_params
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:id_card_number])
+      end
+
+      def after_sign_in_path_for(resource)
+        admin_dashboard_path
+      end
     end
   end
 end

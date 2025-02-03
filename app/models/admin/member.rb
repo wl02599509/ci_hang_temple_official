99be020efb7f796656547f3adcfe6f11cfaccb17
mike <wl02599509@gmail.com>
2025-02-04 00:36:24 +0800

    delete devise default notes

--- app/models/admin/member.rb
+++ app/models/admin/member.rb
@@ -4,8 +4,6 @@ module Admin
 
     PERMISSIONS = { member: 'member', admin: 'admin' }.freeze
 
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 

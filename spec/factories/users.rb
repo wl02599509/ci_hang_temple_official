b012a4350c6c03c1a78c0980d4a95128289ecdd1
Mike <wl02599509@gmail.com>
2025-03-29 16:26:33 +0800

    add seed for user data

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,18 +3,18 @@
 FactoryBot.define do
   factory :user, class: 'User' do
     sequence(:name) { |n| "會員#{n}" }
-    sequence(:id_card_number) { |n| "A1#{n.to_s.rjust(8, '0')}" }
+    sequence(:id_card_number) { |n| "#{('a'..'z').to_a.sample}1#{n.to_s.rjust(8, '0')}" }
     password { 'password123' }
     password_confirmation { 'password123' }
     status { 'general' }
   end
 
   trait :male do
-    id_card_number { "A1#{n.to_s.rjust(8, '0')}" }
+    id_card_number { "#{('a'..'z').to_a.sample}1#{n.to_s.rjust(8, '0')}" }
   end
 
   trait :female do
-    id_card_number { "A2#{n.to_s.rjust(8, '0')}" }
+    id_card_number { "#{('a'..'z').to_a.sample}2#{n.to_s.rjust(8, '0')}" }
   end
 
   User::STATUSES.each do |key, value|

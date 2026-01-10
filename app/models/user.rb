9c771e9537a20907e4a4ddf65d5c1b1158abc407
Mike Lai <mike_lai@morrisonexpress.com>
2026-01-11 00:28:26 +0800

    feat: add user model

---
+++ app/models/user.rb
@@ -0,0 +1,54 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum :sex, { male: 1, female: 2 }, validate: true
+  enum :zodiac, {
+    mice: 1,      # 鼠
+    ox: 2,       # 牛
+    tiger: 3,    # 虎
+    rabbit: 4,   # 兔
+    dragon: 5,   # 龍
+    snake: 6,    # 蛇
+    horse: 7,    # 馬
+    goat: 8,     # 羊
+    monkey: 9,   # 猴
+    rooster: 10, # 雞
+    dog: 11,     # 狗
+    pig: 12      # 豬
+  }, validate: { allow_nil: true }
+  enum :status, {
+    normal: 99
+  }, validate: true
+
+
+  validates :id_number, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  validates :birth_date, presence: true
+  validates :address, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
+
+  # TODO: 另外建立規則進行驗證。
+  validates :id_number, format: { with: /\A[A-Z]/, message: "must start with an uppercase letter" }
+
+  before_validation :set_sex
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def id_number=(value)
+    super(value&.upcase)
+  end
+
+  def set_sex
+    gender_digit = id_number[2].to_i
+    self.sex = gender_digit
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_10_153920) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string "address"
+    t.date "birth_date", null: false
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "encrypted_password", default: "", null: false
+    t.string "id_number", null: false
+    t.string "name", null: false
+    t.string "phone_number", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.integer "sex", null: false
+    t.integer "status", default: 99, null: false
+    t.datetime "updated_at", null: false
+    t.integer "zodiac", comment: "生肖"
+    t.index ["id_number"], name: "index_users_on_id_number", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 end
